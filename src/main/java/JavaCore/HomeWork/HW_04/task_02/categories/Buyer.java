5fcc252417f6cf545bda4037dea39a6d887cdf85
Maksim <i@mpozdnjakov.ru>
2023-11-01 17:18:05 +0300

    HW_04

--- src/main/java/JavaCore/HomeWork/HW_04/task_02/categories/Buyer.java
+++ src/main/java/JavaCore/HomeWork/HW_04/task_02/categories/Buyer.java
@@ -1,4 +1,24 @@
 package JavaCore.HomeWork.HW_04.task_02.categories;
 
+import java.util.Random;
+
 public class Buyer {
+    private int id;
+    private String buyerName;
+    private Random random;
+
+    public Buyer(String buyerName) {
+        this.buyerName = buyerName;
+        this.id = randomId();
+    }
+
+    private int randomId() {
+        random = new Random();
+        return random.nextInt(10000);
+    }
+
+    @Override
+    public String toString() {
+        return buyerName + "(ID " + id + "):";
+    }
 }
