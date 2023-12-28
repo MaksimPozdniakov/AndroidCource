ead3a1f8e02263824278425d4abd08717fa035ce
Maksim <i@mpozdnjakov.ru>
2023-12-28 20:41:15 +0300

    HW5

---
+++ src/main/java/Study/JDK/HomeWorks/HW_05/Fork.java
@@ -0,0 +1,15 @@
+package Study.JDK.HomeWorks.HW_05;
+
+
+public class Fork {
+
+    private boolean condition;
+
+    public boolean isCondition() {
+        return condition;
+    }
+
+    public void setCondition(boolean condition) {
+        this.condition = condition;
+    }
+}
---
+++ src/main/java/Study/JDK/Seminars/Seminar_05/task_01/Program.java
@@ -0,0 +1,7 @@
+package Study.JDK.Seminars.Seminar_05.task_01;
+
+public class Program {
+    public static void main(String[] args) {
+
+    }
+}
