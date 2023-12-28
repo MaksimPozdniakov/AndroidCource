7d2e1aa3b278c14eeef88a55b7eb4ddd23516c87
Maksim <i@mpozdnjakov.ru>
2023-12-29 00:05:18 +0300

    HW5

--- src/main/java/Study/JDK/HomeWorks/HW_05/Main.java
+++ src/main/java/Study/JDK/HomeWorks/HW_05/Main.java
@@ -2,6 +2,7 @@ package Study.JDK.HomeWorks.HW_05;
 
 public class Main {
     public static void main(String[] args) {
-
+        Table table = new Table(5);
+        table.start();
     }
 }
--- src/main/java/Study/JDK/HomeWorks/HW_05/Philosopher.java
+++ src/main/java/Study/JDK/HomeWorks/HW_05/Philosopher.java
@@ -3,7 +3,7 @@ package Study.JDK.HomeWorks.HW_05;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
-public class Philosopher implements Runnable {
+public class Philosopher extends Thread {
 
     private String name;
     private int leftFork;
@@ -13,9 +13,7 @@ public class Philosopher implements Runnable {
     private CountDownLatch countDownLatch;
     private Table table;
 
-    public Philosopher(
-            String name, int leftFork, int rightFork,
-            CountDownLatch countDownLatch, Table table) {
+    public Philosopher(String name, int leftFork, int rightFork, CountDownLatch countDownLatch, Table table) {
 
         this.name = name;
         this.leftFork = leftFork;
@@ -26,10 +24,31 @@ public class Philosopher implements Runnable {
         random = new Random();
     }
 
+    private void eat() throws InterruptedException {
+        if (table.tryGetForks(leftFork, rightFork)) {
+            System.out.println(name + " начал кушать");
+            sleep(500);
+            table.putFork(leftFork, rightFork);
+            System.out.println(name + " Философ покушал и начал думать");
+            countEat++;
+        }
+    }
+
+    private void thinking() throws InterruptedException {
+        sleep(500);
+    }
 
 
     @Override
     public void run() {
-
+        while (countEat < 3) {
+            try {
+                thinking();
+                eat();
+            } catch (InterruptedException e) {
+                e.fillInStackTrace();
+            }
+        }
+        countDownLatch.countDown();
     }
 }
